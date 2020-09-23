5e4e68ddd201df37dc31439835e8257d49a6e874
juan-para <juanpara86@gmail.com>
2020-09-22 21:48:02 -0300

    first commit

---
+++ src/main/java/com/springboot/firstproject/web/firstprojectweb/FirstProjectWebApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.firstproject.web.firstprojectweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjectWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjectWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/firstproject/web/firstprojectweb/FirstProjectWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.firstproject.web.firstprojectweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstProjectWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
