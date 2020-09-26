90c8a6bdfaabfa3762681ccec705c19390b7fc3b
juan-para <juanpara86@gmail.com>
2020-09-26 16:42:17 -0300

    Configuring name as session scope variable

--- src/main/java/com/springboot/firstproject/web/firstprojectweb/controller/TodoController.java
+++ src/main/java/com/springboot/firstproject/web/firstprojectweb/controller/TodoController.java
@@ -5,10 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.springboot.firstproject.web.firstprojectweb.service.TodoService;
 
 @Controller
+@SessionAttributes("name")
 public class TodoController {
 
 	@Autowired
@@ -16,7 +18,8 @@ public class TodoController {
 	
 	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
 	public String showTodosList(ModelMap model) {
-		model.put("todos", service.retrieveTodos("juan"));
+		String name = (String) model.get("name");
+		model.put("todos", service.retrieveTodos(name));
 		return "list-todos";
 	}
 
