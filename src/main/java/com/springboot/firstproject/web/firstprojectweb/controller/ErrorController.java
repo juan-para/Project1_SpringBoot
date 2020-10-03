d0c7d648edf13457a863c7c7ad300475280427fb
juan-para <juanpara86@gmail.com>
2020-10-02 21:20:49 -0300

    Added spring exception handling

---
+++ src/main/java/com/springboot/firstproject/web/firstprojectweb/controller/ErrorController.java
@@ -0,0 +1,23 @@
+package com.springboot.firstproject.web.firstprojectweb.controller;
+
+import java.io.Console;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("error")
+public class ErrorController {
+
+	@ExceptionHandler(Exception.class)
+	private ModelAndView handleException(HttpServletRequest request, Exception ex) {
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("exception",ex.getStackTrace());
+		mv.addObject("url", request.getRequestURL()); 	
+		mv.setViewName("error");
+		return mv;
+	}
+	
+}
--- src/main/webapp/WEB-INF/jsp/error.jsp
+++ src/main/webapp/WEB-INF/jsp/error.jsp
@@ -1,7 +1,6 @@
 <%@ include file="common/header.jspf"%>
 <%@ include file="common/navigation.jspf"%>
 <div class="container pull-left">
-	Welcome ${name}!! <a href="/list-todos">Click here</a> to manage yours
-	todo's
+	An exception occurred! Please contact support.
 </div>
 <%@ include file="common/footer.jspf"%>
